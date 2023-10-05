7fa6f224f4d3ac6dd6acc9efafdb59762bdf9d13
Gabriel Alexandre Rosa <102827318+Gabrielxisney@users.noreply.github.com>
2023-10-05 16:50:33 -0300

    Update TesteDeSoftware.java

--- TesteDeSoftware.java
+++ TesteDeSoftware.java
@@ -7,30 +7,30 @@ public class TesteDeSoftware {
 
     public class User {
         public Connection conectarBD(){
-            Connection conn = null;
-            try{
-            Class.forName("com.mysql.Driver.Manager").newInstance();
+            Connection conn = null;    //1                                                           
+            try{                       //2                                                               
+            Class.forName("com.mysql.Driver.Manager").newInstance();                        
             String url = "jdbc:mysql://127.0.0.1/test?user=lopes&password=123";
-            conn = DriverManager.getConnection(url);
-            }catch (Exception e){}
+            conn = DriverManager.getConnection(url);    
+        }catch (Exception e){}        //3
             return conn; }
     public String nome = "";
 public boolean result = false;
 public boolean verificarUsuario (String login, String senha){
 String sql = "";
-Connection conn = conectarBD();
+Connection conn = conectarBD();    //4
 
 sql += "select nome from usuarios";
 sql += "where login =" + "'" + login + "'";
 sql += " and senha = " + "'" + senha + "';";
-try{
+ try{                            //5
     Statement st = conn.createStatement ();
     ResultSet rs = st.executeQuery(sql);
-    if (rs.next()){
+    if (rs.next()){            //6
         result = true;
         nome = rs.getString ("nome");}
-}catch (Exception e) {}
-return result;
+}catch (Exception e) {}        //7
+return result;                 //8
     }
 
 }
